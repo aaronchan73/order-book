3734ac276d78fb5ffddb8c4116fb18c9f50a8846
Aaron Chan <chan.aaron73@gmail.com>
2023-12-10 00:12:55 -0600

    feat(order): create action enum and order class

---
+++ order.py
@@ -0,0 +1,20 @@
+from enum import Enum
+from datetime import datetime
+
+class Action(Enum):
+    """
+    Buy or sell action
+    """
+    BUY = 1
+    SELL = 2
+
+class Order():
+    """
+    Buy or sell order with a timestamp, price, and volume
+    """
+    def __init__(self, action: Action, price: int, volume: int) -> None:
+        self.action = action
+        self.timestamp = datetime.now()
+        self.price = price
+        self.volume = volume
+
