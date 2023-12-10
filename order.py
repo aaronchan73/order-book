9f596d926c6b1835749603e9760035c4e73a097b
Aaron Chan <chan.aaron73@gmail.com>
2023-12-10 01:11:56 -0600

    feat(order-book): finish add, cancel, place order, and get volume

--- order.py
+++ order.py
@@ -12,7 +12,8 @@ class Order():
     """
     Buy or sell order with a timestamp, price, and volume
     """
-    def __init__(self, action: Action, price: int, volume: int) -> None:
+    def __init__(self, order_id: int, action: Action, price: int, volume: int) -> None:
+        self.order_id = order_id
         self.action = action
         self.timestamp = datetime.now()
         self.price = price
--- orderbook.py
+++ orderbook.py
@@ -1,3 +1,6 @@
+from order import Action, Order
+from collections import deque
+
 class OrderBook():
     """
     Fills buy and sell orders
@@ -11,3 +14,75 @@ class OrderBook():
         self.order_map = {}
         self.volume_map = {}
         self.queue_map = {}
+
+    def cancel_order(self, order: Order) -> None:
+        """
+        Removes order (and potentially queue) from associated heap
+        """
+        if order.order_id in self.order_map:
+            # remove order from queue
+            order = self.order_map[order.order_id]
+            price_queue = self.queue_map[(order.price, order.action)]
+            price_queue.remove(order)
+
+            # if queue is empty, remove from heap
+            if not price_queue:
+                same_book = self.best_bid if order.action == Action.BUY else self.best_ask
+                same_book.remove(price_queue)
+
+            # update dictionaries
+            self.volume_map[(order.price, order.action)] -= order.volume
+            self.order_map.pop(order.order_id)
+
+
+    def add_order_to_book(self, order: Order) -> None:
+        """
+        Updates book's orders and volumes, initializing if does not exist
+        """
+        # price and action does not exist, initialize in dictionaries
+        if not (order.price, order.action) in self.queue_map:
+            self.queue_map[(order.price, order.action)] = deque([order])
+            self.volume_map = [(order.price, order.action)] = order.volume
+        else:
+            self.queue_map[(order.price, order.action)].append(order)
+            self.volume_map[(order.price, order.action)] += order.volume
+
+
+    def place_order(self, order: Order) -> None:
+        """
+        Fills a buy or sell order by matching with opposite order(s)
+        """
+        opposite_book = self.best_ask if order.action == Action.BUY else self.best_bid
+        same_book = self.best_bid if order.action == Action.BUY else self.best_ask
+        self.order_map[order.order_id] = order
+
+        while order.volume > 0 and opposite_book and opposite_book[0][0].price <= order.price:
+            # get price and volume based on matched order
+            matched_order = opposite_book[0][0]
+            trade_price = matched_order.price
+            trade_volume = min(order.volume, matched_order.volume)
+
+            # fill order and remove volume from both buy and sell side
+            order.volume -= trade_volume
+            matched_order.volume -= trade_volume
+
+            if order.action == Action.BUY:
+                print(f"Buy order filled, {trade_volume} shares at {trade_price}.")
+            else:
+                print(f"Sell order filled, {trade_volume} shares at {trade_price}.")
+
+            if not matched_order.volume:
+                self.cancel_order(matched_order)
+
+        if order.volume > 0:
+            self.add_order_to_book(order)
+
+
+    def get_volume_at_price(self, price: int, action: Action):
+        """
+        Returns the volume at a price and action if it exists
+        """
+        if (price, action) in self.volume_map:
+            return self.volume_map[(price, action)]
+        else:
+            return 0
